2e5f3a102ee52a0fc38450686dc2e2a8ebcddd91
Prince Raj J <145485169+programmer-prince@users.noreply.github.com>
2024-12-04 18:55:00 +0530

    Added Day 4 Solution

--- Solutions/python3_the-ai-developer_Plant-Growth-Tracker.py
+++ Solutions/python3_the-ai-developer_Plant-Growth-Tracker.py
@@ -1,3 +1,3 @@
 import math
 N = int(input("Enter No.Of Months: "))
-print(f"No.Of Plants(Predicted!): {int(((1+math.sqrt(5))**N-(1-math.sqrt(5))**N)//(2**N*math.sqrt(5)))}")
+print(f"No.Of Plants(Predicted!): {int(((1+math.sqrt(5))**N-(1-math.sqrt(5))**N)/(2**N*math.sqrt(5)))}")
