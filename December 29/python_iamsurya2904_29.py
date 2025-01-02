f7b3aa039910c8a560c93d04ec8c05f29095339e
iamsurya2904 <128006067+iamsurya2904@users.noreply.github.com>
2025-01-02 23:32:30 +0530

    question 29 completed

--- December 29/python_iamsurya2904_29.py
+++ December 29/python_iamsurya2904_29.py
@@ -36,16 +36,4 @@ def minimum_weight_path(N, M, portals):
 
     return -1
 
-30)
-def super_egg_drop(k: int, n: int) -> int:
-    dp = [[0] * (n + 1) for _ in range(k + 1)]
-    
-    for moves in range(1, n + 1):
-        for eggs in range(1, k + 1):
-            dp[eggs][moves] = dp[eggs - 1][moves - 1] + dp[eggs][moves - 1] + 1
-            if dp[eggs][moves] >= n:
-                return moves
 
-# Example test cases
-print(super_egg_drop(2, 6))  # Output: 3
-print(super_egg_drop(3, 14))  # Output: 4
