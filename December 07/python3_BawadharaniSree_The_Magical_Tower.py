0ae3d4de6d3ab78f34334c7d697ecd83e46e9b62
BawadharaniSree <152498388+BawadharaniSree@users.noreply.github.com>
2025-01-04 00:57:18 +0530

    Day_7

--- December 07/python3_BawadharaniSree_The_Magical_Tower.py
+++ December 07/python3_BawadharaniSree_The_Magical_Tower.py
@@ -1,11 +1,11 @@
 def generate_magical_tower(numRows):
     tower = []
     for i in range(numRows):
-        floor = [1]  # Each floor starts with 1
-        for j in range(1, i):  # Generate inner stones
+        floor = [1]
+        for j in range(1, i):
             floor.append(tower[i-1][j-1] + tower[i-1][j])
         if i > 0:
-            floor.append(1)  # End each floor with 1
+            floor.append(1)
         tower.append(floor)
     return tower
 
