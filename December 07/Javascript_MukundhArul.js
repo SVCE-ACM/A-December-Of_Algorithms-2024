cd91fe0e56ec4cf83eb2585a486e5c053713c273
Mukundh A P <119234753+MukundhArul@users.noreply.github.com>
2025-01-20 17:20:56 +0530

    Create Javascript_MukundhArul.js

---
+++ December 07/Javascript_MukundhArul.js
@@ -0,0 +1,16 @@
+function generatePascalTriangle(numRows) {
+    if (numRows === 0) return [];
+
+    const result = [[1]];
+    for (let i = 1; i < numRows; i++) {
+        const row = [1];
+        for (let j = 1; j < i; j++) {
+            row.push(result[i - 1][j - 1] + result[i - 1][j]);
+        }
+        row.push(1);
+        result.push(row);
+    }
+    return result;
+}
+
+console.log(generatePascalTriangle(5));
