57493d61869ae1ce98b36956cbad6f914ddfb6fe
Praveen Kumar R <133590552+praveen647@users.noreply.github.com>
2025-01-20 17:17:03 +0530

    Create Kotlin_Praveen_Kumar_14.kt

---
+++ December 14/Kotlin_Praveen_Kumar_14.kt
@@ -0,0 +1,19 @@
+import java.util.*
+
+fun canSplitSquad(N: Int, K: Int, D: Int, A: List<Int>): String {
+    val subjectCounts = A.groupingBy { it }.eachCount()
+    val uniqueSubjects = subjectCounts.size
+
+    if (uniqueSubjects < K) {
+        return "NO"
+    }
+
+    val maxTeamSize = (N + D) / 2
+    val minTeamSize = (N - D) / 2
+
+    if (subjectCounts.values.count { it <= minTeamSize } < K) {
+        return "NO"
+    }
+
+    return "YES"
+}
