7fe9a8bbc8c893508bafb149eabdce13da637cf7
DUMBLEDORE01 <152676971+DUMBLEDORE01@users.noreply.github.com>
2025-01-21 21:19:51 +0530

    Add files via upload

---
+++ December 05/5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter the size of the array";
+    int arr[n];
+    cout<<"enter the array elements";
+    for(int i=0;i<n;i++)
+    {
+    	cin>>arr[i];
+	}
+	cout<<"Enter the position to be eleminited";
+	int survivor;
+
+    return 0;
+}
+
